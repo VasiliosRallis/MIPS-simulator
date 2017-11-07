fb36b1aae5fd11695d7ca471554b3cbba55fc8fb
gioannides <gi316@ic.ac.uk>
2017-11-07 01:08:32 +0000

    Reuploading because something went wrong

--- src/R_functions.cpp
+++ src/R_functions.cpp
@@ -1,8 +1,6 @@
 #include "R_functions.hpp"
-#include <limits.h>
 using namespace std;
 
-<<<<<<< HEAD
 bool r_type(bitset<32>& instr, State& mips_state, bool overflow) {
 	bitset<32> funct_field = (instr & bitset<32>(0x0000003F));
 	bitset<32> shamt_field = (instr & bitset<32>(0x000007C0));
@@ -37,42 +35,6 @@ bool r_type(bitset<32>& instr, State& mips_state, bool overflow) {
 			mips_state.reg[index] = sub(mips_state,source1_field,source2_field,overflow);
 	else if(funct_field == bitset<32>(0x00000023))
 			mips_state.reg[index] = subu(mips_state,source1_field,source2_field);
-=======
-void r_type(bitset<32>& instr, State& mips_state) {
-	bitset<32> funct_field = (instr & bitset<32>(0x0000003F));
-	bitset<32> shamt_field = (instr & bitset<32>(0x000007C0));
-	bitset<32> dest_field = (instr & bitset<32>(0x0000F800)); //rd
-	//I think the correct shift is 11 correct me if I'm wrong
-	dest_field >>= 11;
-	unsigned long dest_field2 = dest_field.to_ulong();
-	bitset<32> source2_field = (instr & bitset<32>(0x001F0000)); //rt
-	bitset<32> source1_field = (instr & bitset<32>(0x03E00000)); //rs
-	
-	if(funct_field == bitset<32>(0x00000020))
-			mips_state.reg[dest_field2] = add(mips_state,source1_field,source2_field);
-	else if(funct_field == bitset<32>(0x00000021))
-			mips_state.reg[dest_field] = addu(mips_state,source1_field,source2_field);
-	else if(funct_field == bitset<32>(0x00000024))
-			mips_state.reg[dest_field] = And(mips_state,source1_field,source2_field);
-	else if(funct_field == bitset<32>(0x00000008))
-			jr(mips_state,source1_field);
-	else if(funct_field == bitset<32>(0x00000027))
-			mips_state.reg[dest_field] = nor(mips_state,source1_field,source2_field);
-	else if(funct_field == bitset<32>(0x00000025))
-			mips_state.reg[dest_field] = Or(mips_state,source1_field,source2_field);
-	else if(funct_field == bitset<32>(0x0000002A))
-			mips_state.reg[dest_field] = slt(mips_state,source1_field,source2_field);
-	else if(funct_field == bitset<32>(0x0000002B))
-			mips_state.reg[dest_field] = sltu(mips_state,source1_field,source2_field);
-	else if(funct_field == bitset<32>(0x00000000))
-			mips_state.reg[dest_field] = sll(mips_state,source2_field,shamt_field);
-	else if(funct_field == bitset<32>(0x00000002))
-			mips_state.reg[dest_field] = srl(mips_state,source2_field,shamt_field);
-	else if(funct_field == bitset<32>(0x00000022))
-			mips_state.reg[dest_field] = sub(mips_state,source1_field,source2_field);
-	else if(funct_field == bitset<32>(0x00000023))
-			mips_state.reg[dest_field] = subu(mips_state,source1_field,source2_field);
->>>>>>> d2eeff1f4d5af1854ba360df3b9206292726a77f
 	else if(funct_field == bitset<32>(0x0000001A))
 			div(mips_state,source2_field,source1_field);
 	else if(funct_field == bitset<32>(0x0000001B))
@@ -86,7 +48,6 @@ void r_type(bitset<32>& instr, State& mips_state) {
 	else if(funct_field == bitset<32>(0x00000019))
 			multu(mips_state,source2_field,source1_field);
 	else if(funct_field == bitset<32>(0x00000003))
-<<<<<<< HEAD
 			mips_state.reg[index] = sra(mips_state,source2_field,shamt_field);
 	else if(opcode_field == bitset<32>(0x00000010) && funct_field == bitset<32>(0x00000000)){
 			mfc0(mips_state,source1_field);
@@ -139,46 +100,6 @@ bool slt(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
 	bitset<32> rt = mips_state.reg[source2_field.to_ulong()];
 	if((int)rs.to_ulong() - (int)rt.to_ulong() < 0) {
 		return true;
-=======
-			mips_state.reg[dest_field] = sra(mips_state,source2_field,shamt_field);
-		else
-			return; //this should be changed to the appropriate error code
-	}
-
-bitset<32> add(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
-	 bitset<32> rs = mips_state.reg[source1_field];
-	 bitset<32> rt = mips_state.reg[source2_field];
-	return  rs+rt ;
-}
-
-bitset<32> addu(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
-	ubitset<32>  rs = mips_state.reg[source1_field];
-	ubitset<32>  rt = mips_state.reg[source2_field];
-	return  rs+rt ;
-}
-
-bitset<32> And(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
-	return mips_state.reg[source1_field] & mips_state.reg[source2_field];
-}
-
-void jr(State& mips_state,bitset<32>& source1_field){
-	mips_state.pc = mips_state.reg[source1_field];
-}
-
-bitset<32> nor(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
-	return ~(mips_state.reg[source1_field] | mips_state.reg[source2_field])
-}
-
-bitset<32> Or(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
-	return (mips_state.reg[source1_field] | mips_state.reg[source2_field]);
-}
-
-bool slt(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
-	signed bitset<32> rs = mips_state.reg[source1_field];
-	signed bitset<32> rt = mips_state.reg[source2_field];
-	if(rs - rt < 0) {
-		return 1;
->>>>>>> d2eeff1f4d5af1854ba360df3b9206292726a77f
 	}
 	else {
 		return false;
@@ -186,17 +107,10 @@ bool slt(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
 }
 
 bool sltu(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
-<<<<<<< HEAD
 	bitset<32> rs = mips_state.reg[source1_field.to_ulong()];
 	bitset<32> rt = mips_state.reg[source2_field.to_ulong()];
 	if(rs.to_ulong() < rt.to_ulong()) {
 		return true;
-=======
-	unsigned bitset<32> rs = mips_state.reg[source1_field];
-	unsigned bitset<32> rt = mips_state.reg[source2_field];
-	if(rs - rt < 0) {
-		return 1;
->>>>>>> d2eeff1f4d5af1854ba360df3b9206292726a77f
 	}
 	else {
 		return false;
@@ -204,7 +118,6 @@ bool sltu(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field)
 }
 
 bitset<32> sll(State& mips_state,bitset<32>& source2_field,bitset<32> shamt_field){
-<<<<<<< HEAD
 	return (mips_state.reg[source2_field.to_ulong()] << shamt_field.to_ulong());
 }
 
@@ -259,78 +172,19 @@ void mult(State& mips_state,bitset<32>& source2_field,bitset<32>& source1_field)
 	bitset<32> rs = mips_state.reg[source1_field.to_ulong()];
 	bitset<32> rt = mips_state.reg[source2_field.to_ulong()];
 	int64_t result  = (int)rs.to_ulong() * (int)rt.to_ulong();
-=======
-	return (mips_state.reg[source2_field] << shamt_field);
-}
-
-bitset<32> srl(State& mips_state,bitset<32>& source2_field,bitset<32> shamt_field){
-	ubitset<32> result = mips_state.reg[source2_field];
-	return (result >> shamt_field);
-}
-
-bitset<32> sub(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
-	signed bitset<32> rs = mips_state.reg[source1_field];
-	signed bitset<32> rt = mips_state.reg[source2_field];
-	return  rs-rt ;
-}
-
-bitset<32> subu(State& mips_state,bitset<32>& source1_field,bitset<32>& source2_field){
-	ubitset<32> rs = mips_state.reg[source1_field];
-	ubitset<32> rt = mips_state.reg[source2_field];
-	return  rs-rt ;
-}
-
-void div(State& mips_state,bitset<32>& source2_field,bitset<32>& source1_field){
-	signed bitset<32> rs = mips_state.reg[source1_field];
-	signed bitset<32> rt = mips_state.reg[source2_field];
-	mips_state.Hi = rs % rt;
-	mips_state.Lo = rs / rt;
-}
-
-void divu(State& mips_state,bitset<32>& source2_field,bitset<32>& source1_field){
-	unsigned bitset<32> rs = mips_state.reg[source1_field];
-	unsigned bitset<32> rt = mips_state.reg[source2_field];
-	mips_state.Hi = rs % rt;
-	mips_state.Lo = rs / rt;
-}
-
-void mfhi(State& mips_state,bitset<32>& dest_field){
-	mips_state.reg[dest_field] = mips_state.Hi;
-}
-
-void mflo(State& mips_state,bitset<32>& dest_field){
-	mips_state.reg[dest_field] = mips_state.Lo;
-}
-
-void mult(State& mips_state,bitset<32>& source2_field,bitset<32>& source1_field){
-	signed bitset<32> rs = mips_state.reg[source1_field];
-	signed bitset<32>	rt = mips_state.reg[source2_field];
-	signed int64_t result  = rs * rt;
->>>>>>> d2eeff1f4d5af1854ba360df3b9206292726a77f
 	mips_state.Hi = (result >> 32);
 	mips_state.Lo = (result << 32) >> 32;
 }
 
 void multu(State& mips_state,bitset<32>& source2_field,bitset<32>& source1_field){
-<<<<<<< HEAD
 	bitset<32> rs = mips_state.reg[source1_field.to_ulong()];
 	bitset<32> rt = mips_state.reg[source2_field.to_ulong()];
 	uint64_t result = rs.to_ulong() * rt.to_ulong();
-=======
-	ubitset<32> rs = mips_state.reg[source1_field];
-	ubitset<32> rt = mips_state.reg[source2_field];
-	uint64_t result = rs * rt;
->>>>>>> d2eeff1f4d5af1854ba360df3b9206292726a77f
 	mips_state.Hi = (result >> 32);
 	mips_state.Lo = (result << 32) >> 32;
 }
 
 bitset<32> sra(State& mips_state,bitset<32>& source2_field,bitset<32> shamt_field){
-<<<<<<< HEAD
 	bitset<32> result = mips_state.reg[source2_field.to_ulong()];
 	return ((int)result.to_ulong() >> shamt_field.to_ulong());
-=======
-	signed bitset<32> result = mips_state.reg[source2_field];
-	return (result >> shamt_field);
->>>>>>> d2eeff1f4d5af1854ba360df3b9206292726a77f
 }
