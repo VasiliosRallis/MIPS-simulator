61e9d8f612865f73179ea88f11957ca35885af1b
gioannides <gi316@ic.ac.uk>
2017-11-11 02:29:31 +0000

    Fixed the R-type functions, on my PC they compiled

--- include/R_functions.hpp
+++ include/R_functions.hpp
@@ -2,34 +2,31 @@
 #define R_TYPE
 #include <iostream>
 #include "mips.hpp"
-#include <fstream>
-#include <string>
-#include <bitset>
 #include <stdint.h>
 using namespace std;
 
 
-bool r_type(int32_t& instr,State& mips_state,bool overflow);
+bool r_type(int32_t& instr,State& mips_state);
 
 
-int32_t add(State& mips_state,int32_t& source1_field,int32_t& source2_field, bool overflow);
-int32_t addu(State& mips_state,int32_t& source1_field,int32_t& source2_field);
-int32_t And(State& mips_state,int32_t& source1_field,int32_t& source2_field);
-void jr(State& mips_state,int32_t& source1_field);
-int32_t nor(State& mips_state,int32_t& source1_field,int32_t& source2_field);
-int32_t Or(State& mips_state,int32_t& source1_field,int32_t& source2_field);
-bool slt(State& mips_state,int32_t& source1_field,int32_t& source2_field);
-bool sltu(State& mips_state,int32_t& source1_field,int32_t& source2_field);
-int32_t sll(State& mips_state,int32_t& source2_field,int32_t shamt_field);
-int32_t srl(State& mips_state,int32_t& source2_field,int32_t shamt_field);
-int32_t sub(State& mips_state,int32_t& source1_field,int32_t& source2_field, bool overflow);
-int32_t subu(State& mips_state,int32_t& source1_field,int32_t& source2_field);
-void div(State& mips_state,int32_t& source2_field,int32_t& source1_field);
-void divu(State& mips_state,int32_t& source2_field,int32_t& source1_field);
-void mfhi(State& mips_state,int32_t& dest_field);
-void mflo(State& mips_state,int32_t& dest_field);
-void mult(State& mips_state,int32_t& source2_field,int32_t& source1_field);
-void multu(State& mips_state,int32_t& source2_field,int32_t& source1_field);
-int32_t sra(State& mips_state,int32_t& source2_field,int32_t shamt_field);
+int32_t add(State& mips_state,uint32_t source1_field,uint32_t source2_field, bool& overflow);
+uint32_t addu(State& mips_state,uint32_t source1_field,uint32_t source2_field);
+int32_t And(State& mips_state,uint32_t source1_field,uint32_t source2_field);
+void jr(State& mips_state,uint32_t source1_field);
+int32_t nor(State& mips_state,uint32_t source1_field,uint32_t source2_field);
+int32_t Or(State& mips_state,uint32_t source1_field,uint32_t source2_field);
+bool slt(State& mips_state,uint32_t source1_field,uint32_t source2_field);
+bool sltu(State& mips_state,uint32_t source1_field,uint32_t source2_field);
+int32_t sll(State& mips_state,uint32_t source2_field,uint32_t shamt_field);
+int32_t srl(State& mips_state,uint32_t source2_field,uint32_t shamt_field);
+int32_t sub(State& mips_state,uint32_t source1_field,uint32_t source2_field, bool& overflow);
+uint32_t subu(State& mips_state,uint32_t source1_field,uint32_t source2_field);
+void div(State& mips_state,uint32_t source2_field,uint32_t source1_field);
+void divu(State& mips_state,uint32_t source2_field,uint32_t source1_field);
+void mfhi(State& mips_state,uint32_t dest_field);
+void mflo(State& mips_state,uint32_t dest_field);
+void mult(State& mips_state,uint32_t source2_field,uint32_t source1_field);
+void multu(State& mips_state,uint32_t source2_field,uint32_t source1_field);
+int32_t sra(State& mips_state,uint32_t source2_field,uint32_t shamt_field);
 
 #endif
