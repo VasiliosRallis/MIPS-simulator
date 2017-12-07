6a83bf1e7136994c125d94c1a1b9ae770ae89ecd
gioannides <gi316@ic.ac.uk>
2017-12-07 01:12:56 +0000

    Fixed one of the two failing tests

--- testbench/tests/src/hard_set.s
+++ testbench/tests/src/hard_set.s
@@ -1,10 +1,10 @@
 .set noat
 .set noreorder
-
+	
+        lui $1,21845
+	ori $1,$1,21845
 	lui $3,0
 	ori $3,$3,0
-	lui $1,21845
-	ori $1,$1,21845
 	lui $4,52428
 	ori $4,$4,52428
 	or $1,$4,$1
@@ -18,6 +18,6 @@
 	sra $1,$1,1
 	lui $8,50244
 	ori $8,$8,17477
-	xor $10,$1,$8
-	addi $2,$10,0		# $2 = 1 is the result
+	xor $9,$1,$8
+	addi $2,$9,0		# $2 = 1 is the result
 	jr $0
